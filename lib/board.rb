639b657d019926db26c2db68032a0f3cbc7f1f52
DJacobson17 <davidbjacobson@gmail.com>
2022-11-17 09:33:20 -0600

    added horizontal win check

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 require_relative 'symbols'
+require 'pry-byebug'
 
 class Board
 
   include Symbols
 
   attr_reader :grid
-  
+
   def initialize
     @grid = Array.new(6) { Array.new(7) { empty_space } }
   end
@@ -16,6 +17,35 @@ class Board
     end
     puts (1..7).to_a.join(' ')
   end
+
+  def update_board(move, player)
+    @grid.reverse_each do |row|
+      next unless row[move] == empty_space
+
+      row[move] = player.color
+      return @grid.find_index(row)
+    end
+  end
+
+  def check_win(row, column, color)
+    check_vertical(row, column, color)
+  end
+
+  def check_vertical(row, column, color)
+    return if row > 2
+
+    @grid[row][column] == color && @grid[row + 1][column] == color && @grid[row + 2][column] == color && @grid[row + 3][column] == color
+  end
+
+  def check_horizontal(row, color)
+    @grid[row].each_index do |index|
+      # binding.pry
+      next unless color == @grid[row][index] && color == @grid[row][index + 1] && color == @grid[row][index + 2] && color == @grid[row][index + 3]
+
+      return true
+    end
+    false
+  end
 end
 
-  Board.new.display_board
+
